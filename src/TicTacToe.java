55b3c8cc7b03abfdd2f61bc07f68fa6c55532797
diazken <dm.anthony1991@gmail.com>
2025-06-02 20:06:28 +0200

    condition x wins or o wins

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -99,7 +99,119 @@ public class TicTacToe implements ActionListener{
     }
 
     public void check(){
+        //Check X conditions
+        if((buttons[0].getText()=="X") &&
+           (buttons[1].getText()=="X") &&
+           (buttons[2].getText()=="X")
+        ){
+            xWins(0,1,2); //winning combination
+        }
 
+        if((buttons[3].getText()=="X") &&
+           (buttons[4].getText()=="X") &&
+           (buttons[5].getText()=="X")
+        ){
+            xWins(3,4,5); //winning combination
+        }
+
+        if((buttons[6].getText()=="X") &&
+           (buttons[7].getText()=="X") &&
+           (buttons[8].getText()=="X")
+        ){
+            xWins(6,7,8); //winning combination
+        }
+
+        if((buttons[0].getText()=="X") &&
+           (buttons[3].getText()=="X") &&
+           (buttons[6].getText()=="X")
+        ){
+            xWins(0,3,6); //winning combination
+        }
+
+        if((buttons[1].getText()=="X") &&
+           (buttons[4].getText()=="X") &&
+           (buttons[7].getText()=="X")
+        ){
+            xWins(1,4,7); //winning combination
+        }
+
+        if((buttons[2].getText()=="X") &&
+           (buttons[5].getText()=="X") &&
+           (buttons[8].getText()=="X")
+        ){
+            xWins(2,5,8); //winning combination
+        }
+
+        if((buttons[0].getText()=="X") &&
+           (buttons[4].getText()=="X") &&
+           (buttons[8].getText()=="X")
+        ){
+            xWins(0,4,8); //winning combination
+        }
+
+        if((buttons[2].getText()=="X") &&
+           (buttons[4].getText()=="X") &&
+           (buttons[6].getText()=="X")
+        ){
+            xWins(2,4,6); //winning combination
+        }
+
+        //Check O conditions
+        if((buttons[0].getText()=="O") &&
+                (buttons[1].getText()=="O") &&
+                (buttons[2].getText()=="O")
+        ){
+            oWins(0,1,2); //winning combination
+        }
+
+        if((buttons[3].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[5].getText()=="O")
+        ){
+            oWins(3,4,5); //winning combination
+        }
+
+        if((buttons[6].getText()=="O") &&
+                (buttons[7].getText()=="O") &&
+                (buttons[8].getText()=="O")
+        ){
+            oWins(6,7,8); //winning combination
+        }
+
+        if((buttons[0].getText()=="O") &&
+                (buttons[3].getText()=="O") &&
+                (buttons[6].getText()=="O")
+        ){
+            oWins(0,3,6); //winning combination
+        }
+
+        if((buttons[1].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[7].getText()=="O")
+        ){
+            oWins(1,4,7); //winning combination
+        }
+
+        if((buttons[2].getText()=="O") &&
+                (buttons[5].getText()=="O") &&
+                (buttons[8].getText()=="O")
+        ){
+            oWins(2,5,8); //winning combination
+        }
+
+        if((buttons[0].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[8].getText()=="O")
+        ){
+            oWins(0,4,8); //winning combination
+        }
+
+        if((buttons[2].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[6].getText()=="O")
+        ){
+            oWins(2,4,6); //winning combination
+        }
     }
 
     public void xWins(int a, int b, int c){
